df3255e85c1f9091a99829aad6a3bc7b772ba3c3
Vika <vickypimkie@ukr.net>
2018-01-28 23:18:50 +0200

    about added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './layout/header/header.component';
+
 import { NewComponent } from './layout/new/new.component';
 import { AboutComponent } from './layout/about/about.component';
 import { MenuComponent } from './layout/menu/menu.component';
 import { HomeComponent } from './layout/home/home.component';
-import { NotFoundPageComponent } from './layout/not-found-page/not-found-page.component';
-import { BasketComponent } from './layout/basket/basket.component';
 import { ContactComponent } from './layout/contact/contact.component';
+import { ReservComponent } from './layout/reserv/reserv.component';
+import { GalleryComponent } from './layout/gallery/gallery.component';
 
 import { NgxCarouselModule } from 'ngx-carousel';
 import 'hammerjs';
@@ -18,24 +18,23 @@ const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'basket', component: BasketComponent },
+  { path: 'reserv', component: ReservComponent },
   { path: 'menu', component: MenuComponent },
-  { path: 'new', component: NewComponent },
+  { path: 'gallery', component: GalleryComponent },
   { path: 'home', component: HomeComponent },
-  { path: '**', component: NotFoundPageComponent },
+  { path: '**', component: HomeComponent },
  ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     NewComponent,
     AboutComponent,
     MenuComponent,
     HomeComponent,
-    NotFoundPageComponent,
-    BasketComponent,
-    ContactComponent
+    ContactComponent,
+    GalleryComponent,
+    ReservComponent
 ],
   imports: [
     BrowserModule,
--- src/app/layout/menu/menu.component.ts
+++ src/app/layout/menu/menu.component.ts
@@ -10,30 +10,118 @@ export class MenuComponent  {
   menus: Model.Menu[];
   items: Model.Product[];
 
-  icecreams: Model.Icecream[];
-
   constructor() {
     this.menus = [
       {
       id: 1,
-      name: 'Все',
+      name: 'Мороженое',
       items: [
         {
           id: 1,
-          name: 'Леша',
-          note: 'no'
+          name: 'Ванильное',
+          note: 'sl.png'
+        },
+        {
+          id: 2,
+          name: 'Шоколадное',
+          note: 'mt.png'
+        },
+        {
+          id: 3,
+          name: 'Фисташковое',
+          note: 'fis.png'
+        },
+        {
+          id: 4,
+          name: 'Ежевика-лаванда',
+          note: 'lav.png'
+        },
+        {
+          id: 5,
+          name: 'Шоколадно-мятное',
+          note: 'mt.png'
+        },
+        {
+          id: 6,
+          name: 'Свежие фрукты',
+          note: 'fr.png'
+        },
+        {
+          id: 7,
+          name: 'Клубничное',
+          note: 'kl.png'
+        },
+        {
+          id: 8,
+          name: 'Лайм',
+          note: 'lime.png'
+        },
+        {
+          id: 9,
+          name: 'Ореховое',
+          note: 'or.png'
+        },
+        {
+          id: 10,
+          name: 'Фитнес сорбеты',
+          note: 'sorb.png'
+        },
+        {
+          id: 11,
+          name: 'Мармеладные мишки гамми',
+          note: 'gam.png'
+        },
+        {
+          id: 12,
+          name: 'Мороженое фондю',
+          note: 'fon.png'
         }
       ]
 
       },
       {
         id: 2,
-        name: 'Мороженое',
+        name: 'Топинги',
         items: [
           {
             id: 1,
-            name: 'Дима',
-            note: 'no'
+            name: 'Шоколад',
+            note: 't2.png'
+          },
+          {
+            id: 2,
+            name: 'Вишня',
+            note: 't4.png'
+          },
+          {
+            id: 3,
+            name: 'Карамель',
+            note: 't1.png'
+          },
+          {
+            id: 4,
+            name: 'Миндаль',
+            note: 't3.png'
+          },
+          {
+            id: 5,
+            name: 'Кокос',
+            note: 't1.png'
+          },
+          {
+            id: 6,
+            name: 'Малина',
+            note: 't3.png'
+          },
+          {
+            id: 7,
+            name: 'Банан-киви',
+            note: 't2.png'
+          },
+          {
+            id: 8,
+            name: 'Манго',
+            note: 't1.png'
           }
         ]
 
@@ -44,18 +132,48 @@ export class MenuComponent  {
         items: [
           {
             id: 1,
-            name: 'Леша',
+            name: 'Шоколадный',
+            note: 'no'
+          },
+          {
+            id: 2,
+            name: 'Ванильный',
+            note: 'no'
+          },
+          {
+            id: 3,
+            name: 'Клубничный',
+            note: 'no'
+          },
+          {
+            id: 4,
+            name: 'С малиной и киви',
             note: 'no'
           }
         ]
       },
       {
         id: 4,
-        name: 'Топинги',
+        name: 'Гонконгские вафли',
         items: [
           {
             id: 1,
-            name: 'Леша',
+            name: 'С бананом и киви',
+            note: 'no'
+          },
+          {
+            id: 2,
+            name: 'С вишней',
+            note: 'no'
+          },
+          {
+            id: 3,
+            name: 'С клубникой',
+            note: 'no'
+          },
+          {
+            id: 4,
+            name: 'С шоколадом и орехами',
             note: 'no'
           }
         ]
@@ -67,7 +185,22 @@ export class MenuComponent  {
         items: [
           {
             id: 1,
-            name: 'Леша',
+            name: 'Соки',
+            note: 'no'
+          },
+          {
+            id: 2,
+            name: 'Молочный коктейль',
+            note: 'no'
+          },
+          {
+            id: 3,
+            name: 'Гляссе',
+            note: 'no'
+          },
+          {
+            id: 4,
+            name: 'Лимонад',
             note: 'no'
           }
         ]
@@ -79,7 +212,42 @@ export class MenuComponent  {
         items: [
           {
             id: 1,
-            name: 'Леша',
+            name: 'Чай фруктовый',
+            note: 'no'
+          },
+          {
+            id: 2,
+            name: 'Чай зеленый',
+            note: 'no'
+          },
+          {
+            id: 3,
+            name: 'Чай черный',
+            note: 'no'
+          },
+          {
+            id: 4,
+            name: 'Американо',
+            note: 'no'
+          },
+          {
+            id: 5,
+            name: 'Эспрессо',
+            note: 'no'
+          },
+          {
+            id: 6,
+            name: 'Капуччино',
+            note: 'no'
+          },
+          {
+            id: 7,
+            name: 'Латте',
+            note: 'no'
+          },
+          {
+            id: 8,
+            name: 'Мокко',
             note: 'no'
           }
         ]
@@ -89,7 +257,7 @@ export class MenuComponent  {
     this.showPart(this.menus[0]);
   }
 
-  showPart(menu){
+  showPart(menu) {
     this.items = menu.items;
   }
 
